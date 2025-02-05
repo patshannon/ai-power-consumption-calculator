1d9e77100b1946b1f129b5429e3b6b48099c3f71
patshannon <peshannon104@gmail.com>
2025-02-04 22:38:01 -0400

    init, layout, math etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,81 @@
-import Image from "next/image";
-
+'use client';
+import { ChevronDoubleUpIcon } from '@heroicons/react/16/solid';
+import { ChangeEvent, FormEvent, useState } from 'react';
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [input, setInput] = useState(0);
+  const [response, setResponse] = useState<any>({});
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleOnSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const resp = await calculateTotal(input);
+    if (resp) {
+      setResponse(resp);
+    }
+  };
+  const calculateTotal = async (number: number) => {
+    const wh = number * 2.9;
+    const gCO2 = 481 * (wh / 1000);
+    return {
+      wh: wh,
+      microwaveSeconds: number * 15,
+      gCO2: gCO2,
+    };
+  };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(parseInt(e.target.value));
+  };
+  return (
+    <div className="text-center">
+      <h1 className="mx-auto text-4xl font-extrabold lg:text-6xl lg:max-w-2xl">AI Power Consumption Calculator</h1>
+      <p className="mt-10 font-bold lg:text-lg">Examples of AI queries:</p>
+      <ul className="text-sm lg:text-base">
+        <li>ChatGPT</li>
+        <li>Google Search</li>
+        <li>Amazon</li>
+        <li>Gemini</li>
+      </ul>
+      <form onSubmit={handleOnSubmit} className="mx-auto mt-10 flex flex-col max-w-60">
+        <label htmlFor="number">Daily number of AI queries:</label>
+        <input
+          id="number"
+          min={0}
+          value={input}
+          onChange={handleChange}
+          name="number"
+          className="mt-1 text-xl lg:text-2xl p-2 text-center bg-transparent border border-white rounded-full"
+          type="number"
+          placeholder="0"
+        />
+        <button className="transition hover:shadow-lg hover:shadow-green-500/25 mt-3 bg-green-700 pointer rounded-full text-xl p-2">Calculate</button>
+      </form>
+      {response.wh ? (
+        <div>
+        <div className="mt-10 p-5 text-3xl lg:text-4xl text-left font-extrabold rounded-lg shadow-2xl shadow-green-600 border border-green-500/15 w-max mx-auto">
+          <div>
+            {response.wh?.toFixed(2)} <span className="text-sm">Wh</span>
+          </div>
+          <div>
+            {response?.gCO2?.toFixed(2)}{' '}
+            <span className="text-sm">
+              gCO<sub>2</sub>
+            </span>
+          </div>
+          <div>
+            {(((response?.gCO2?.toFixed(2) * 365) / 1000) * 2.204).toFixed(2)}{' '}
+            <span className="text-sm">
+              lbsCO<sub>2</sub> / year
+            </span>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <div className='mt-10 flex items-center justify-center'>
+            <ChevronDoubleUpIcon className='w-6 h-6 text-red-500'/>
+            <div className='text-xl font-bold'>{(((((9600.00+((((response?.gCO2 * 365) / 1000) * 2.204))-9600)/9600)))*100).toFixed(2)}% yearly</div>
+          </div>
+          <div className='text-sm'>*Based on 9600 <span className='text-xs'>lbsC0<sub>2</sub> / year</span> average</div>
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   );
 }
